33928a3fb277286097d738df09aa8312af2920b6
Mafruza <artiqbaevamafruza@gmail.com>
2026-03-12 08:46:24 +0500

    changed

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
   return (
     <section className="w-full sm:min-h-[600px] md:h-screen lg:h-screen bg-gradient-to-r from-[#F9FAFB] to-[#F5F4F7]">
       <div className="mx-auto relative h-full px-[10px] md:pl-[60px] md:pt-[14px] lg:pl-[215px] lg:pt-[50px] flex flex-col">
-        <div className="relative  sm:text-wrap  z-10  md:max-w-[1320px] lg:max-w-[620px] max-w-[500px]">
-          <h1 className="text-[24px] md:text-[20px] px-[20px] md:px-0 leading-[30px] md:leading:[18px] lg:leading-[46px] lg:text-[32px] mt-[110px] md:mt-[20px] lg:mt-[70px] text-center md:text-left lg:text-left font-semibold text-[#030712]">
+        <div className="relative  sm:text-wrap  z-10  md:max-w-[1320px] lg:max-w-[620px] max-w-[330px] mx-auto">
+          <h1 className="text-[24px] md:text-[20px] leading-[30px] md:leading:[18px] lg:leading-[46px] lg:text-[32px] mt-[110px] md:mt-[20px] lg:mt-[70px] text-center md:text-left lg:text-left font-semibold text-[#030712]">
             {isRu ? (
               <>
                 <span className="text-[#1463E1]">{t("Hero", "title_highlight")}</span>
