5875719a903958b3d809b96ac2c8b8ad6d07e901
Mafruza <artiqbaevamafruza@gmail.com>
2026-03-12 17:49:36 +0500

    changed

--- src/app/components/CTA.tsx
+++ src/app/components/CTA.tsx
@@ -26,7 +26,7 @@ export default function CTA() {
             ),
             titleKey: "security",
             descKey: "security_desc",
-            offset: "lg:-translate-x-8"
+            offset: "md:-translate-x-6 lg:-translate-x-8"
         },
         {
             icon: (
@@ -41,36 +41,42 @@ export default function CTA() {
     return (
         <section id="contact" className="my-[60px] md:my-[80px] lg:mt-[64px] lg:mb-[80px] bg-white">
             <div className="max-w-[380px] px-[6px] md:px-0 md:max-w-[648px] lg:max-w-[1090px] mx-auto ">
-                <div className="bg-[#F9FAFB] mb-4 text-center md:text-left lg:text-left rounded-[16px] p-6 lg:p-8 grid lg:grid-cols-2 gap-12 lg:gap-2 items-center">
+                <div className="bg-[#F9FAFB] mb-4 text-center md:text-left lg:text-left rounded-[16px] md:rounded-[24px] lg:rounded-[16px] p-6 md:p-8 lg:p-8 grid md:grid-cols-2 lg:grid-cols-2 gap-8 md:gap-4 lg:gap-2 items-center">
                     <div>
-                        <h2 className="text-[20px] md:text-[32px] max-w-[480px] tracking-[0.02em] sm:text-3xl lg:text-[32px] font-semibold text-[#030712] leading-[1.3] lg:leading-[42px]">
+                        <h2 className="text-[20px] md:text-[20px] max-w-[480px] tracking-[0.02em] sm:text-3xl lg:text-[32px] font-semibold text-[#030712] leading-[1.3] lg:leading-[42px]">
                             {t("CTA", "title")}
                         </h2>
-                        <p className="mt-[16px] text-[#1E2939] tracking-[0.02em] font-regular md:text-[14px] sm:text-base leading-[22px] hidden md:block">
+                        <p className="mt-[16px] text-[#1E2939] tracking-[0.02em] font-regular md:text-[14px] sm:text-base leading-[22px] hidden lg:block">
                             {t("CTA", "description")}
                         </p>
 
-                        <p className="mt-[16px] text-[#1E2939] tracking-[0.02em] font-regular text-base leading-[22px] md:hidden">
+                        <p className="mt-[16px] text-[#1E2939] text-[14px] tracking-[0.02em] font-regular text-base leading-[16px] lg:hidden">
                             {t("CTA", "descriptionMobile")}
                         </p>
 
-                        <div className="mt-16 flex md:flex-row flex-col gap-5 sm:flex-col ">
+                        <div className="mt-8 md:mt-10 lg:mt-16 flex flex-col md:flex-row gap-4 md:gap-2 lg:gap-5">
                             <button
                                 onClick={() => setModalMode("demo")}
-                                className="inline-flex cursor-pointer items-center justify-center px-[28px] py-[13px] rounded-[32px] text-[16px] text-nowrap leading-[20px] font-medium text-white bg-[#1463E1]"
+                                className="inline-flex md:hidden lg:inline-flex cursor-pointer items-center justify-center px-[28px] md:px-[16px] lg:px-[28px] py-[13px] md:py-[10px] lg:py-[13px] rounded-[32px] text-[16px] md:text-[14px] lg:text-[16px] text-nowrap leading-[20px] font-medium text-white bg-[#1463E1]"
                             >
                                 {t("CTA", "button1")}
                             </button>
                             <button
+                                onClick={() => setModalMode("demo")}
+                                className="hidden md:inline-flex lg:hidden cursor-pointer items-center justify-center px-[28px] md:px-[16px] lg:px-[28px] py-[13px] md:py-[10px] lg:py-[13px] rounded-[32px] text-[16px] md:text-[14px] lg:text-[16px] text-nowrap leading-[20px] font-medium text-white bg-[#1463E1]"
+                            >
+                                {t("CTA", "buttonlaptop1")}
+                            </button>
+                            <button
                                 onClick={() => setModalMode("consultation")}
-                                className="inline-flex cursor-pointer items-center justify-center px-[28px] py-[13px] rounded-[32px] text-[16px] text-nowrap leading-[20px] font-medium text-[#1E2939] bg-white "
+                                className="inline-flex cursor-pointer items-center justify-center px-[28px] md:px-[16px] lg:px-[28px] py-[13px] md:py-[10px] lg:py-[13px] rounded-[32px] text-[16px] md:text-[14px] lg:text-[16px] text-nowrap leading-[20px] font-medium text-[#1E2939] bg-white "
                             >
                                 {t("CTA", "button2")}
                             </button>
                         </div>
                     </div>
 
-                    <div className="flex flex-col gap-[12px]  md:ml-auto lg:ml-auto w-full lg:max-w-[292px] sm:min-w-[300px]">
+                    <div className="flex flex-col gap-[12px] md:ml-auto lg:ml-auto w-full md:max-w-[260px] lg:max-w-[292px] sm:min-w-[260px]">
                         {features.map((feature, index) => (
                             <div
                                 key={index}
@@ -83,7 +89,7 @@ export default function CTA() {
                                     <h3 className="text-[20px] font-semibold text-[#030712] leading-[24px] tracking-[0.02em] ">
                                         {t("CTA", feature.titleKey)}
                                     </h3>
-                                    <p className="mt-[8px] text-[12px] text-[#4A5565] text-[#4A5565] leading-[16px] font-medium">
+                                    <p className="mt-[8px] text-[12px] text-[#4A5565] leading-[16px] font-medium">
                                         {t("CTA", feature.descKey)}
                                     </p>
                                 </div>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -17,25 +17,25 @@ export default function Footer() {
         <footer className="bg-white max-w-[380px]  md:max-w-[648px] lg:max-w-[1090px] mx-auto py-[32px]">
             <div className="sm:p-3 md:p-1 lg:p-0 px-[6px] md:px-0 flex flex-col lg:flex-row gap-6">
 
-                <div className="bg-[#1C68E1] rounded-[16px] p-[32px] text-white flex flex-col justify-between w-full lg:w-[350px] min-h-[300px]">
-                    <div>
+                <div className="bg-[#1C68E1] rounded-[16px] p-[32px] md:p-[40px] lg:p-[32px] text-white flex flex-col md:flex-row lg:flex-col justify-between md:items-end lg:items-start w-full lg:w-[350px] min-h-[300px] md:min-h-[180px] lg:min-h-[300px]">
+                    <div className="md:max-w-[340px] lg:max-w-none">
                         <Image src={logofooter} alt="Logo Footer" width={170} height={32} />
-                        <p className="text-[#EEF8FF] mt-[16px] font-medium text-[16px] leading-[22px]">
+                        <p className="text-[#EEF8FF] mt-[16px] md:mt-[24px] lg:mt-[16px] font-medium text-[16px] md:text-[18px] lg:text-[16px] leading-[22px] md:leading-[26px] lg:leading-[22px]">
                             {t("Footer", "description")}
                         </p>
                     </div>
-                    <div className="flex gap-3 mt-12">
+                    <div className="flex gap-3 mt-12 md:mt-0 lg:mt-12">
 
                         <Link href="https://www.facebook.com/">
-                            <Image src={Facebook} alt="Facebook" width={24} height={24} />
+                            <Image src={Facebook} alt="Facebook" width={24} height={24} className="md:w-[32px] md:h-[32px] lg:w-[24px] lg:h-[24px]" />
                         </Link>
 
                         <Link href="https://www.instagram.com/">
-                            <Image src={Insatgram} alt="Instagram" width={24} height={24} />
+                            <Image src={Insatgram} alt="Instagram" width={24} height={24} className="md:w-[32px] md:h-[32px] lg:w-[24px] lg:h-[24px]" />
                         </Link>
 
                         <Link href="https://www.linkedin.com/">
-                            <Image src={LinkedIn} alt="LinkedIn" width={24} height={24} />
+                            <Image src={LinkedIn} alt="LinkedIn" width={24} height={24} className="md:w-[32px] md:h-[32px] lg:w-[24px] lg:h-[24px]" />
                         </Link>
 
                     </div>
